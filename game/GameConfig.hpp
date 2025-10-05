70fa88b1195f4b02b4dbf75ad3dee6cdcd0a411f
SeshuHomePc <seshu.basava70@gmail.com>
2025-10-05 22:10:25 +0200

    Change player size to 512

--- game/GameConfig.hpp
+++ game/GameConfig.hpp
@@ -4,7 +4,7 @@ namespace GameConfig {
 constexpr int cLogicalWidth = 1920;
 constexpr int cLogicalHeight = 1080;
 constexpr int cExpectedFrameRate = 120;
-constexpr int cPlayerSize = 128;
+constexpr int cPlayerSize = 512;
 constexpr int cEnemysize = 16;
 constexpr int cPlayerSpeed = 20;
 
